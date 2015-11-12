9740dbc845c54bb3cbdfd0149e0d7ea0c54877ca
Josh Gubler <josh_gubler@byu.edu>
2015-11-12 15:00:45 -0700

    rolling update works

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -7,14 +7,9 @@ ssh adm.jgubler@tabura1.byu.edu scp rc.yml atom1-prd:
 ssh adm.jgubler@tabura1.byu.edu scp svc.yml atom1-prd:
 
 ssh adm.jgubler@tabura1.byu.edu ssh atom1-prd << EOF
-#if rc doesn't exist
-#...create rc
-kubectl create -f rc.yml
-#else
-#...tell rc to update pods
+
 kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
-#if service doesn't exist
-#...create service
+kubectl create -f rc.yml
 kubectl create -f svc.yml
 
 EOF
