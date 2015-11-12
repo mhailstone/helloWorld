24f7b3c07343a64f820624e4e0e7e40a078889fc
mhailstone <matthew.hailstone@gmail.com>
2015-11-12 16:07:51 -0700

    ssh and scp commands fixed

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -ex
 
-#copy files to k8s server
-scp rc.yml adm.mdh26@tabura1.byu.edu:
-scp svc.yml adm.mdh26@tabura1.byu.edu:
-ssh adm.mdh26@tabura1.byu.edu scp rc.yml atom1-prd:
-ssh adm.mdh26@tabura1.byu.edu scp svc.yml atom1-prd:
+mkdir -p ~/.ssh/
+echo -e $PRIVATE_TABURA_SSH_KEY > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
-ssh adm.mdh26@tabura1.byu.edu ssh atom1-prd << EOF
+#copy files to k8s server
+scp -P 22222 -o StrictHostKeyChecking=no rc.yml adm.mdh26@tabura1.byu.edu:
+scp -P 22222 -o StrictHostKeyChecking=no svc.yml adm.mdh26@tabura1.byu.edu:
+ssh -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu scp -i ~/.ssh/codeship_rsa rc.yml atom1-prd:
+ssh -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu scp -i ~/.ssh/codeship_rsa svc.yml atom1-prd:
+
+ssh -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu ssh -i ~/.ssh/codeship_rsa shatom1-prd << EOF
 
 kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
 kubectl create -f rc.yml
