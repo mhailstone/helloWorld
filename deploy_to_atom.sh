35610e6e27d594499cb2921ef32a997fd25e7ab9
Josh Gubler <josh_gubler@byu.edu>
2015-11-12 14:41:35 -0700

    deploy to atom

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -ex
 
-ssh adm.jgubler@tabura1.byu.edu ssh dockeru@atom1-prd kubectl get pods
+#copy files to k8s server
+scp rc.yml adm.jgubler@tabura1.byu.edu:
+scp svc.yml adm.jgubler@tabura1.byu.edu:
+ssh adm.jgubler@tabura1.byu.edu scp rc.yml atom1-prd:
+ssh adm.jgubler@tabura1.byu.edu scp svc.yml atom1-prd:
+
+ssh adm.jgubler@tabura1.byu.edu ssh atom1-prd << EOF
+#if rc doesn't exist
+#...create rc
+kubectl create -f rc.yml
+#else
+#...tell rc to update pods
+kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
+#if service doesn't exist
+#...create service
+kubectl create -f svc.yml
+
+EOF
