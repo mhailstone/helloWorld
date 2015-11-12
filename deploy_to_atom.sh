f6d673d3352c760948d446a866350f40cf1c6d9f
mhailstone <matthew.hailstone@gmail.com>
2015-11-12 15:53:41 -0700

    Encrypted files ready

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
 #copy files to k8s server
-scp rc.yml adm.jgubler@tabura1.byu.edu:
-scp svc.yml adm.jgubler@tabura1.byu.edu:
-ssh adm.jgubler@tabura1.byu.edu scp rc.yml atom1-prd:
-ssh adm.jgubler@tabura1.byu.edu scp svc.yml atom1-prd:
+scp rc.yml adm.mdh26@tabura1.byu.edu:
+scp svc.yml adm.mdh26@tabura1.byu.edu:
+ssh adm.mdh26@tabura1.byu.edu scp rc.yml atom1-prd:
+ssh adm.mdh26@tabura1.byu.edu scp svc.yml atom1-prd:
 
-ssh adm.jgubler@tabura1.byu.edu ssh atom1-prd << EOF
+ssh adm.mdh26@tabura1.byu.edu ssh atom1-prd << EOF
 
 kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
 kubectl create -f rc.yml
