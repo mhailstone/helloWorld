e372b11880f61a7a76d68a41169d6c3fe6ccdcfd
mhailstone <matthew.hailstone@gmail.com>
2015-11-13 09:09:43 -0700

    Modified scp commands inside tabura

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -7,8 +7,8 @@ chmod 600 ~/.ssh/id_codeship_rsa
 #copy files to k8s server
 scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no rc.yml adm.mdh26@tabura1.byu.edu:
 scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no svc.yml adm.mdh26@tabura1.byu.edu:
-ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu scp -i ~/.ssh/codeship_rsa rc.yml atom1-prd:
-ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu scp -i ~/.ssh/codeship_rsa svc.yml atom1-prd:
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu "scp -i ~/.ssh/codeship_rsa rc.yml dockeru@atom1-prd.byu.edu:"
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu "scp -i ~/.ssh/codeship_rsa svc.yml dockeru@atom1-prd.byu.edu:"
 
 ssh -i ~/.ssh/id_codeship_rsa -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu ssh -i ~/.ssh/codeship_rsa atom1-prd << EOF
 
