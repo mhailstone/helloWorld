8a5625ec3b0de8b1c4cb520c2a497a80ea9442a4
mhailstone <matthew.hailstone@gmail.com>
2015-11-13 11:15:19 -0700

    This is going to work man!

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -7,15 +7,8 @@ chmod 600 ~/.ssh/id_codeship_rsa
 #copy files to k8s server
 scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no rc.yml adm.mdh26@tabura.byu.edu:
 scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no svc.yml adm.mdh26@tabura.byu.edu:
+scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no kubectl-commands.sh adm.mdh26@tabura.byu.edu:
 ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu "scp -i ~/.ssh/codeship_rsa rc.yml dockeru@atom1-prd.byu.edu:"
 ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu "scp -i ~/.ssh/codeship_rsa svc.yml dockeru@atom1-prd.byu.edu:"
-
-ssh -i ~/.ssh/id_codeship_rsa -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu ssh -i ~/.ssh/codeship_rsa dockeru@atom1-prd.byu.edu << EOF
-
-kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
-kubectl create -f rc.yml
-kubectl create -f svc.yml
-
-rm rc.yml
-rm svc.yml
-EOF
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu "scp -i ~/.ssh/codeship_rsa kubectl-commands.sh dockeru@atom1-prd.byu.edu:"
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu 'ssh -i ~/.ssh/codeship_rsa dockeru@atom1-prd.byu.edu "./kubectl-commands.sh"'
---
+++ kubectl-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
+kubectl create -f rc.yml
+kubectl create -f svc.yml
+
+rm rc.yml
+rm svc.yml
+rm kubectl-commands.sh
