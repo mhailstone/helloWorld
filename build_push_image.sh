ee929f43a09f99ee47451fa55e9e27733999cb64
mhailstone <matthew.hailstone@gmail.com>
2015-11-13 08:17:54 -0700

    Changed how the shell files are invoked.

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
 echo zero
-#mkdir -p ~/.docker
-#echo one
-#echo -e $DOCKERCFG | base64 -d > ~/.docker/config.json
-#echo two
-#docker build -t hub-tmp.byu.edu/jgubler/hello-world:latest .
-#echo three
-#docker push hub-tmp.byu.edu/jgubler/hello-world:latest
-#echo four
+mkdir -p ~/.docker
+echo one
+echo -e $DOCKERCFG | base64 -d > ~/.docker/config.json
+echo two
+docker build -t hub-tmp.byu.edu/jgubler/hello-world:latest .
+echo three
+docker push hub-tmp.byu.edu/jgubler/hello-world:latest
+echo four
