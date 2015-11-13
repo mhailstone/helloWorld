0d78cf7e0f0a2df19c8dc960ad60be3cf5b745fc
mhailstone <matthew.hailstone@gmail.com>
2015-11-13 10:22:03 -0700

    Modified scp commands inside tabura

--- deploy_to_atom.sh
+++ deploy_to_atom.sh
@@ -5,12 +5,12 @@ echo -e $PRIVATE_TABURA_SSH_KEY > ~/.ssh/id_codeship_rsa
 chmod 600 ~/.ssh/id_codeship_rsa
 
 #copy files to k8s server
-scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no rc.yml adm.mdh26@tabura1.byu.edu:
-scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no svc.yml adm.mdh26@tabura1.byu.edu:
-ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu "scp -i ~/.ssh/codeship_rsa rc.yml dockeru@atom1-prd.byu.edu:"
-ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu "scp -i ~/.ssh/codeship_rsa svc.yml dockeru@atom1-prd.byu.edu:"
+scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no rc.yml adm.mdh26@tabura.byu.edu:
+scp -i ~/.ssh/id_codeship_rsa -P 22222 -o StrictHostKeyChecking=no svc.yml adm.mdh26@tabura.byu.edu:
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu "scp -i ~/.ssh/codeship_rsa rc.yml dockeru@atom1-prd.byu.edu:"
+ssh -i ~/.ssh/id_codeship_rsa -p 22222 -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu "scp -i ~/.ssh/codeship_rsa svc.yml dockeru@atom1-prd.byu.edu:"
 
-ssh -i ~/.ssh/id_codeship_rsa -o StrictHostKeyChecking=no adm.mdh26@tabura1.byu.edu ssh -i ~/.ssh/codeship_rsa atom1-prd << EOF
+ssh -i ~/.ssh/id_codeship_rsa -o StrictHostKeyChecking=no adm.mdh26@tabura.byu.edu ssh -i ~/.ssh/codeship_rsa dockeru@atom1-prd.byu.edu << EOF
 
 kubectl rolling-update hello-world-controller --image=hub-tmp.byu.edu/jgubler/hello-world:latest
 kubectl create -f rc.yml
